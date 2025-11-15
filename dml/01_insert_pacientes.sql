afdc22718d634386b96a4af8459c104106571b4c
Paulo <henriqyepaulo@gmail.com>
2025-11-15 12:05:02 -0300

    Update 01_insert_pacientes.sql

--- dml/01_insert_pacientes.sql
+++ dml/01_insert_pacientes.sql
@@ -1,1 +1,5 @@
-
+insert into pacientes( Nome, data_nascimento) 
+values 
+    ('Vanusa Soares De Mendes', '1990-02-20'), 
+    ('Pietra Varotti De Galvão', '2004-10-22'), 
+    ('Juliana Vesperdiano Falcão', '1998-04-23')
