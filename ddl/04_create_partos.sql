62e45a31446cbeb282c20c459a70e98c563015be
Paulo <henriqyepaulo@gmail.com>
2025-11-14 23:49:27 -0300

    Create partos table with relevant fields

--- ddl/04_create_partos.sql
+++ ddl/04_create_partos.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE partos (
+id_parto INT AUTO_INCREMENT PRIMARY KEY,
+data_parto DATE NOT NULL,
+hora TIME NOT NULL,
+tipo_parto Varchar(15) Check (tipo_parto in ('Normal','Cesarea')),
+id_medico INT,
+id_paciente INT
+);
