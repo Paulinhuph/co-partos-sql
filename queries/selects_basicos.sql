a5e061faadd2902a2e45528cc67e67c00606725b
Paulo <henriqyepaulo@gmail.com>
2025-11-15 13:39:18 -0300

    Update selects_basicos.sql

--- queries/selects_basicos.sql
+++ queries/selects_basicos.sql
@@ -1,5 +1,18 @@
+-- SELECT SIMPLES:
 SELECT * FROM pacientes;
 SELECT nome, data_nascimento FROM pacientes;
-
 SELECT * FROM medicos;
-SELECT crm, especialidade
+SELECT crm, especialidade * FROM medicos
+
+-- SELECT WHERE:
+SELECT * FROM partos
+WHERE tipo_parto = 'Normal';
+
+-- SELECT ORDER BY
+SELECT * FROM partos
+ORDER BY data_parto DESC;
+
+-- SELECT LIMIT
+SELECT * FROM pacientes
+ORDER BY id_paciente
+LIMIT 2;
