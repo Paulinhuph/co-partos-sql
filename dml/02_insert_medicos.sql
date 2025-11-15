7212bbfbf29e3dd684708e4c0b5c82d9116e7ec6
Paulo <henriqyepaulo@gmail.com>
2025-11-15 12:10:14 -0300

    Update 02_insert_medicos.sql

--- dml/02_insert_medicos.sql
+++ dml/02_insert_medicos.sql
@@ -1,1 +1,6 @@
+INSERT INTO medicos (crm, especialidade)
+VALUES
+    ('123456-SP', 'Ginecologista e Obstetra'),
+    ('125456-SP', 'Ginecologista e Obstetra'),
+    ('112156-SP', 'Ginecologista e Obstetra');
 
