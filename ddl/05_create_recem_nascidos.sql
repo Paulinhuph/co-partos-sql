d04c6410a98cd2fa3eb07123a3eb623ac729a4d2
Paulo <henriqyepaulo@gmail.com>
2025-11-15 11:11:06 -0300

    Update 05_create_recem_nascidos.sql

--- ddl/05_create_recem_nascidos.sql
+++ ddl/05_create_recem_nascidos.sql
@@ -1,1 +1,7 @@
+CREATE TABLE recem_nascido (
+    id_parto INT PRIMARY KEY,
+    sexo CHAR(1) CHECK (sexo IN ('M','F')),
+    peso FLOAT NOT NULL,
+    FOREIGN KEY (id_parto) REFERENCES partos(id_parto)
+);
 
