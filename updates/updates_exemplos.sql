47f2f61467f536686912030fcc13e984c6dba41b
Paulo <henriqyepaulo@gmail.com>
2025-11-15 13:49:32 -0300

    Update updates_exemplos.sql

--- updates/updates_exemplos.sql
+++ updates/updates_exemplos.sql
@@ -1,1 +1,14 @@
+-- UPDATE 1 (ATUALIZAR PESO DO RN, EM CASO DE ERROS DE DIGITAÇÃO)
+UPDATE recem_nascido
+SET peso = 3.200
+WHERE id_parto = 1;
 
+-- UPDATE 2: (ATUALIZAR ESPECIALIDADE DO MÉDICO)
+UPDATE medicos
+SET especialidade = 'Obstetra'
+WHERE id_medico = 2;
+
+-- UPDATE 3: (ATUALIZAR OU CORRIGIR O TIPO DE PARTO)
+UPDATE partos
+SET tipo_parto = 'Cesarea'
+WHERE id_parto = 3;
