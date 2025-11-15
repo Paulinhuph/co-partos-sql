17bdb3fc90dc402259680a725ce046094a225d88
Paulo <henriqyepaulo@gmail.com>
2025-11-15 12:17:00 -0300

    Update 03_insert_partos.sql

--- dml/03_insert_partos.sql
+++ dml/03_insert_partos.sql
@@ -1,1 +1,13 @@
+INSERT INTO partos (data_parto, hora, tipo_parto, id_medico, id_paciente)
+VALUES
+    ('2025-11-15', '12:25:00', 'Normal', 1, 1);
+
+INSERT INTO partos (data_parto, hora, tipo_parto, id_medico, id_paciente)
+VALUES
+    ('2025-11-16', '08:40:00', 'Cesarea', 2, 2);
+
+INSERT INTO partos (data_parto, hora, tipo_parto, id_medico, id_paciente)
+VALUES
+    ('2025-11-17', '19:15:00', 'Normal', 1, 3);
+
 
