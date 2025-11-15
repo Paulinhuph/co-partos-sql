d60cebf753eb2cdb539547e9ec14ef89e012a832
Paulo <henriqyepaulo@gmail.com>
2025-11-15 11:56:04 -0300

    Add foreign key constraints to partos table

--- ddl/04_create_partos.sql
+++ ddl/04_create_partos.sql
@@ -1,8 +1,10 @@
 CREATE TABLE partos (
-id_parto INT AUTO_INCREMENT PRIMARY KEY,
-data_parto DATE NOT NULL,
-hora TIME NOT NULL,
-tipo_parto Varchar(15) Check (tipo_parto in ('Normal','Cesarea')),
-id_medico INT,
-id_paciente INT
+    id_parto INT AUTO_INCREMENT PRIMARY KEY,
+    data_parto DATE NOT NULL,
+    hora TIME NOT NULL,
+    tipo_parto VARCHAR(15) CHECK (tipo_parto IN ('Normal','Cesarea')),
+    id_medico INT,
+    id_paciente INT,
+    FOREIGN KEY (id_medico) REFERENCES medicos(id_medico),
+    FOREIGN KEY (id_paciente) REFERENCES pacientes(id_paciente)
 );
