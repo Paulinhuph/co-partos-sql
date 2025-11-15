716cef1669361cf6a30c025e656c1a07a8b8dd3d
Paulo <henriqyepaulo@gmail.com>
2025-11-15 12:22:01 -0300

    Update 04_insert_recem_nascido.sql

--- dml/04_insert_recem_nascido.sql
+++ dml/04_insert_recem_nascido.sql
@@ -1,1 +1,12 @@
+INSERT INTO recem_nascido (sexo, peso, id_parto)
+VALUES
+    ('M', 2.756, 1);
+
+INSERT INTO recem_nascido (sexo, peso, id_parto)
+VALUES
+    ('M', 3.566, 2);
+
+INSERT INTO recem_nascido (sexo, peso, id_parto)
+VALUES
+    ('F', 4.100, 3);
 
